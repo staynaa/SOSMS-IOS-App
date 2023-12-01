6cefb75a3f0f249d87f37de10a601798becef0c8
Stayna <staynaa.cs@gmail.com>
2023-12-01 01:47:00 -0500

    edited alert page

--- SOSMS App/SOSMS App/Views/AlertView.swift
+++ SOSMS App/SOSMS App/Views/AlertView.swift
@@ -11,22 +11,10 @@ struct AlertView: View {
     var body: some View {
         NavigationView{
             VStack{
-                VStack(spacing: 20){
-                    Text("Send A Quick Alert")
-                        .fontWeight(.bold)
-                        .foregroundStyle(.red)
-                    Text("Send your contacts a quick alert without a message to immediately Notify them of your current situation.")
-                    Text("Your LAST PINNED location will be SENT.")
-                        .fontWeight(.semibold)
-                        .underline()
-                }
-                VStack(spacing: 20){
-                    Text("Send An Alert With Message")
-                        .fontWeight(.bold)
-                        .foregroundStyle(.orange)
-                    Text("Send your contacts a message with details on your current situation. You can toggle if you want to send your location as well.")
-                }
-
+                AlertButtonsView(buttonTitle: "Send A Quick Alert", buttonDesc: "Send your contacts a quick alert without a message to immediately Notify them of your current situation.")
+                
+                AlertButtonsView(buttonTitle: "Send An Alert With Message", buttonDesc: "Send your contacts a message with details on your current situation. You can toggle if you want to send your location as well.")
+                
             }.navigationTitle("Alert")
         }
     }
